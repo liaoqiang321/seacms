6ad6505816c9c472697881b09a167c7208dcffc7
liaoqiang <1083488361@qq.com>
2020-07-04 14:05:20 +0800

    初始化

---
+++ data/admin_097a402e544fe46687fe4397.php
@@ -0,0 +1,1 @@
+<?php $admin_path = 'dc03do'; ?>
---
+++ data/common.inc.php
@@ -0,0 +1,9 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = '172.17.0.2';
+$cfg_dbname = 'seacms';
+$cfg_dbuser = 'root';
+$cfg_dbpwd = '123456';
+$cfg_dbprefix = 'sea_';
+$cfg_db_language = 'utf8';
+?>
